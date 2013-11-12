196c08d27a31caf95ca759a9a6ccf5344f28e9a0
fstakem <fstakem@gmail.com>
2013-11-12 10:43:49 -0500

    Got the nav bar working and broke the page into components

--- App.py
+++ App.py
@@ -4,15 +4,19 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-@app.route('/landing/')
-def landingPage():
-    view = request.args.get('view')
+@app.route('/')
+def landing():
+    view = 'landing'
     
     return render_template('landing.html', view=view)
+
+@app.route('/graph/')
+def graph():
+    view = 'graph'
+    graph = request.args.get('graph')
+    
+    return render_template('graph.html', view=view, graph=graph)
 
 if __name__ == "__main__":
     app.run()
