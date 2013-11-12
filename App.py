00ec84a5471fc6840fab345af3cbe54ae1238fbf
Fred Stakem <fstakem@gmail.com>
2013-11-12 00:23:14 -0500

    Added some bootstrap styling

---
+++ App.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/landing/')
+def landingPage():
+    view = request.args.get('view')
+    
+    return render_template('landing.html', view=view)
+
+if __name__ == "__main__":
+    app.run()
